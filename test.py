ec0d3ba51b1c26c45dffbd804fb2dbc97514d0a4
Nagendra <45304960+nagendrakumarallada@users.noreply.github.com>
2026-05-21 18:26:23 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-print(len(input("enter name")))
+print(len(input("enter name ")))
 
-print(len(input("Enter the Series of Numbers")))
+print(len(input("Enter the Series of Numbers ")))
+
+
+numbers = input("Enter numbers separated by space: ")
+
+numbers_list = numbers.split()
+
+total = sum(map(int, numbers_list))
+
+print("Sum of numbers:", total)
