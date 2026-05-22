b225a4cc3917f037bf67a59cbe14eab3ae3f74a5
Nagendra <45304960+nagendrakumarallada@users.noreply.github.com>
2026-05-22 16:44:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -10,3 +10,26 @@ numbers_list = numbers.split()
 total = sum(map(int, numbers_list))
 
 print("Sum of numbers:", total)
+
+# Lists #
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print(fruits)
+
+# tuple #
+coordinates = (17.3850, 78.4867)
+
+print(coordinates)
+# sets #
+numbers = {1,2,3,3,4}
+
+print(numbers)
+
+# Dictionaries #
+employee = {
+    "name": "Nagendra",
+    "id": 101,
+    "role": "Developer"
+}
+
+print(employee["name"])
